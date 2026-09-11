562248bc894a5cf852bb58fcea52a6dae0dde522
TRASH-PROGRAMMER <e1316318565@live.uleam.edu.ec>
2026-09-11 08:01:44 -0500

    feat: add repository abstractions for statistics snapshots and user devices

--- backend/src/domain/repositories/statistics_snapshots_repository.py
+++ backend/src/domain/repositories/statistics_snapshots_repository.py
@@ -1,16 +1,19 @@
 from abc import ABC, abstractmethod
 from typing import List, Optional
-from src.domain.entities.statistics_snapshots import StatisticsSnapshot
+from src.domain.entities.statistics_snapshots import StatisticsSnapshots
 
 class StatisticsSnapshotRepository(ABC):
+    """
+    Puerto de persistencia para instantáneas de estadísticas.
+    """
     @abstractmethod
-    def save(self, statistics_snapshot: StatisticsSnapshot) -> StatisticsSnapshot:
+    def save(self, statistics_snapshot: StatisticsSnapshots) -> StatisticsSnapshots:
         raise NotImplementedError
     @abstractmethod
-    def get_by_id(self, statistics_snapshot_id: str) -> Optional[StatisticsSnapshot]:
+    def get_by_id(self, statistics_snapshot_id: str) -> Optional[StatisticsSnapshots]:
         raise NotImplementedError
     @abstractmethod
-    def get_for_user(self, user_id: str) -> List[StatisticsSnapshot]:
+    def get_for_user(self, user_id: str) -> List[StatisticsSnapshots]:
         raise NotImplementedError
     @abstractmethod
     def delete(self, statistics_snapshot_id: str) -> bool:
--- backend/src/domain/repositories/user_device_repository.py
+++ backend/src/domain/repositories/user_device_repository.py
@@ -1,16 +1,16 @@
 from abc import ABC, abstractmethod
 from typing import List, Optional
-from src.domain.entities.user_devices import UserDevice
+from src.domain.entities.user_devices import UserDevices
 
 class UserDeviceRepository(ABC):
     @abstractmethod
-    def save(self, user_device: UserDevice) -> UserDevice:
+    def save(self, user_device: UserDevices) -> UserDevices:
         raise NotImplementedError
     @abstractmethod
-    def get_by_id(self, user_device_id: str) -> Optional[UserDevice]:
+    def get_by_id(self, user_device_id: str) -> Optional[UserDevices]:
         raise NotImplementedError
     @abstractmethod
-    def get_for_user(self, user_id: str) -> List[UserDevice]:
+    def get_for_user(self, user_id: str) -> List[UserDevices]:
         raise NotImplementedError
     @abstractmethod
     def delete(self, user_device_id: str) -> bool:
